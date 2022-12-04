12e96d800c420e574f5c9e5fadf2b8a1373ff6fc
Jesper Larsson <jlars.19@gmail.com>
2022-12-04 22:37:44 +0100

    day4 solutions

--- day4/Solution1.js
+++ day4/Solution1.js
@@ -4,8 +4,8 @@ const isIncluded = (range, part) => {
   const [partStart, partEnd] = part.split("-").map((str) => parseInt(str));
   return rangeStart <= partStart && rangeEnd >= partEnd;
 };
-const assignments = Input1.split("\n").map((line) => {
+const isIncludedAssingments = Input1.split("\n").map((line) => {
   const [elf1, elf2] = line.split(",");
   return isIncluded(elf1, elf2) || isIncluded(elf2, elf1) ? 1 : 0;
 });
-console.log(assignments.reduce((a, b) => a + b, 0));
+console.log(isIncludedAssingments.reduce((a, b) => a + b, 0));
