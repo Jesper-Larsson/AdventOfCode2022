5cf6f49e673a1c5ca81531c657ae442d7d711abc
Jesper Larsson <jesper.a.larsson@sogeti.com>
2022-12-08 15:45:26 +0100

    day8 in progress...

--- day8/Solution1.js
+++ day8/Solution1.js
@@ -1,13 +1,8 @@
 import Input from "./Input.js";
 const cells = Input.split("\n").map((line) => line.split(""));
-const isVisible = cells.map((row) => row.map((cell) => 0));
+const isVisible = cells.map((row) => row.map(() => 0));
 for (let row = 0; row < cells.length; row++) {
-    //isVisible[row] = [];
-    for (let col = 0; col < cells.length; col++) {
-        console.log("------------");
-        console.log("checking r: " + row + " col: " + col);
-
-        //edges
+    for (let col = 0; col < cells[0].length; col++) {
         if (
             col === 0 ||
             col === cells[0].length - 1 ||
@@ -17,68 +12,18 @@ for (let row = 0; row < cells.length; row++) {
             isVisible[row][col] = 1;
             continue;
         }
-        //right
-        let maxSize = 0;
-        for (let colToCheck = 0; colToCheck < col; colToCheck++) {
-            if (cells[row][colToCheck] > maxSize) {
-                console.log("max: " + cells[row][colToCheck]);
-                console.log("checked col: " + colToCheck);
-                maxSize = cells[row][colToCheck];
-            }
-        }
-        if (cells[row][col] > maxSize) {
-            isVisible[row][col] = 1;
-            continue;
-        }
-        //left
-        maxSize = 0;
-        for (let colToCheck = cells[0].length - 1; colToCheck > col; colToCheck--) {
-            if (cells[row][colToCheck] > maxSize) {
-                maxSize = cells[row][colToCheck];
-            }
-        }
-        if (cells[row][col] > maxSize) {
-            isVisible[row][col] = 1;
-            continue;
-        }
-        //down
-        maxSize = 0;
-        for (let rowToCheck = 0; rowToCheck < row; rowToCheck++) {
-            if (cells[rowToCheck][col] > maxSize) {
-                maxSize = cells[rowToCheck][col];
-            }
-        }
-        if (cells[row][col] > maxSize) {
-            isVisible[row][col] = 1;
-            continue;
-        }
-        //up
-        maxSize = 0;
-        for (let rowToCheck = row.length - 1; rowToCheck > row; rowToCheck--) {
-            if (cells[rowToCheck][col] > maxSize) {
-                maxSize = cells[rowToCheck][col];
-            }
-        }
-        if (cells[row][col] > maxSize) {
+        const maxLeft = Math.max(...cells[row].slice(0, col));
+        const maxRight = Math.max(...cells[row].slice(col + 1));
+        const maxAbove = Math.max(
+            ...cells.map((rowToEval) => rowToEval[col]).slice(0, row)
+        );
+        const maxBelow = Math.max(
+            ...cells.map((rowToEval) => rowToEval[col]).slice(row + 1)
+        );
+        if (cells[row][col] > Math.min(maxLeft, maxRight, maxAbove, maxBelow)) {
             isVisible[row][col] = 1;
         }
     }
 }
 const rowTotals = isVisible.map((row) => row.reduce((a, b) => a + b, 0));
-rowTotals.forEach((row) => {
-    console.log(row);
-});
-
-for (let r = 0; r < cells.length; r++) {
-    for (let c = 0; c < cells[0].length; c++) {
-        process.stdout.write("" + isVisible[r][c]);
-    }
-    console.log("");
-}
-console.log("----");
-for (let r = 0; r < cells.length; r++) {
-    for (let c = 0; c < cells[0].length; c++) {
-        process.stdout.write("" + cells[r][c]);
-    }
-    console.log("");
-}
+console.log(rowTotals.reduce((a, b) => a + b, 0));
