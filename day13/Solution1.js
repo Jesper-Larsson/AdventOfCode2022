a2abe3b438fe82df1eaca888168304354cf7d688
Jesper Larsson <jlars.19@gmail.com>
2022-12-13 21:55:28 +0100

    day13 solutions

---
+++ day13/Solution1.js
@@ -0,0 +1,34 @@
+import Input from "./Input.js";
+let pairs = Input.split("\n\n");
+const indiciesWhereCorrect = [];
+const getSmallestItem = (item1, item2) => {
+  if (item2 === undefined) {
+    return 1;
+  }
+  if (typeof item1 === "number" && typeof item2 === "number") {
+    return item1 === item2 ? -1 : item1 < item2 ? 1 : 2;
+  }
+  const leftArr = typeof item1 === "number" ? [item1] : item1;
+  const rightArr = typeof item2 === "number" ? [item2] : item2;
+  for (let i = 0; i < leftArr.length; i++) {
+    if (i === rightArr.length) {
+      return 2;
+    }
+    switch (getSmallestItem(leftArr[i], rightArr[i])) {
+      case 2:
+        return 2;
+      case 1:
+        return 1;
+      default:
+        break;
+    }
+  }
+  return rightArr.length > leftArr.length ? 1 : -1;
+};
+pairs.forEach((pair, index) => {
+  let [left, right] = pair.split("\n").map((arrStr) => eval(arrStr));
+  if (getSmallestItem(left, right) < 2) {
+    indiciesWhereCorrect.push(index + 1);
+  }
+});
+console.log(indiciesWhereCorrect.reduce((a, b) => a + b, 0));
---
+++ day13/Solution2.js
@@ -0,0 +1,34 @@
+import Input from "./Input.js";
+let lines = Input.split("\n").filter((line) => line !== "");
+lines.push("[[2]]");
+lines.push("[[6]]");
+const getSmallestItem = (item1, item2) => {
+  if (item2 === undefined) {
+    return 1;
+  }
+  if (typeof item1 === "number" && typeof item2 === "number") {
+    return item1 === item2 ? -1 : item1 < item2 ? 1 : 2;
+  }
+  const leftArr = typeof item1 === "number" ? [item1] : item1;
+  const rightArr = typeof item2 === "number" ? [item2] : item2;
+  for (let i = 0; i < leftArr.length; i++) {
+    if (i === rightArr.length) {
+      return 2;
+    }
+    switch (getSmallestItem(leftArr[i], rightArr[i])) {
+      case 2:
+        return 2;
+      case 1:
+        return 1;
+      default:
+        break;
+    }
+  }
+  return rightArr.length > leftArr.length ? 1 : -1;
+};
+lines.sort((line1, line2) => {
+  const left = eval(line1);
+  const right = eval(line2);
+  return getSmallestItem(left, right) === 2 ? 1 : -1;
+});
+console.log((lines.indexOf("[[2]]") + 1) * (lines.indexOf("[[6]]") + 1));
